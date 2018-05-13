fe5b1628f9fd688212f29cfb2c70c12420f760ce
student17 <student17@pcoip.ki.agh.edu.pl>
2018-05-13 11:44:14 +0200

    1st commit

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,7 +1,9 @@
 package com.company.enroller.persistence;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -17,6 +19,41 @@ public class ParticipantService {
 
 	public Collection<Participant> getAll() {
 		return connector.getSession().createCriteria(Participant.class).list();
+	}
+
+	public Participant findByLogin(String login) {
+		
+		return (Participant) connector.getSession().get(Participant.class, login); 
+		
+	}
+
+	public void create(Participant participant) {
+		
+	Transaction tmpTransaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+    tmpTransaction.commit();
+		
+		
+	}
+
+	public void delete(Participant tmpParticipant) {
+	
+	Transaction tmpTransaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(tmpParticipant);
+    tmpTransaction.commit();
+		
+	}
+
+	public void update(Participant participant) {
+	
+	Participant tmpParticipant = (Participant) connector.getSession().get(Participant.class, participant.getLogin()); 
+	
+	Transaction tmpTransaction = connector.getSession().beginTransaction();
+		tmpParticipant.setPassword(participant.getPassword());
+		connector.getSession().save(tmpParticipant);
+    tmpTransaction.commit();
+		
+		
 	}
 
 }
